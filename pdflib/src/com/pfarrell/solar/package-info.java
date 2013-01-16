9aadd6c7d45e8fda1e1dc39c14b4c275c5353030
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 16:17:36 -0500

    put into git at github

---
+++ pdflib/src/com/pfarrell/solar/package-info.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2009-2011 Patrick Farrell  and R. Scott Eisenberg. All Rights reserved.
+ */
+/**
+ * Classes that calculate times and places for solar events, such as runrise and sunset.
+ */
+package com.pfarrell.solar;
