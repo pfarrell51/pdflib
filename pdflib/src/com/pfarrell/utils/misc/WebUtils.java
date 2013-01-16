f2e623c0e20b9919ed87977944fa64fcbd9de84a
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 16:17:37 -0500

    put into git at github

---
+++ pdflib/src/com/pfarrell/utils/misc/WebUtils.java
@@ -0,0 +1,70 @@
+/*
+ * @(#)WebUtils.java	0.51 1999/07/14
+ *
+ * Copyright (c) 2004, Pat Farrell, All rights reserved.
+ * This code will be released with a suitable Open Source
+ * license, probably BSD-like.
+ * based on work Copyright (c) 2001, OneBigCD, Inc.  All rights reserved.
+ */
+
+
+package com.pfarrell.utils.misc;
+ 
+import java.lang.Math.*;
+
+
+/**
+ * handy dandy web/jsp utilities. Abstract since all functions are static.
+ * @author  Scott Eisenberg, Brian Boesch, and others
+ * @version 0.51, 1999/07/14
+ */
+public abstract class WebUtils {
+    
+    
+    /**
+     *  Given a string, this method remove all the non digits and return that
+     *  string.  For example "123-456-789a" would return "123456789".  This
+     *  will return a null if null arg passed in.
+     * @param arg - String to parse digits from
+     * @return string without non-digits.
+     */
+    public static String parseOnlyDigits (String arg)
+    {
+        if (arg == null) return null;
+        StringBuffer rval = new StringBuffer (arg.length());
+        for (int i = 0; i < arg.length(); i++)
+        {
+            char c = arg.charAt(i);
+            if (Character.isDigit(c))
+                rval.append(c);
+        }
+        return rval.toString();
+    }
+
+    
+/** simple table, no letters Eye or OH to confuse users. */
+private static char b34enctab[] = { '0','1','2','3','4','5','6','7','8','9', 
+            'A','B','C','D','E','F','G','H','J','K','L','M','N','P',
+              'Q','R','S','T','U','V','W','X','Y','Z'};
+/**
+ * convert byte buffer to easy to type alphanumeric values.
+ * Uses just upper case letters and digits. Put last nibble
+ * in the middle because it will always be a digit.
+ * @param buf incomming buffer
+ * @return encoded string
+ */
+public static String base32encode(byte[] buf) {
+    StringBuffer sb = new StringBuffer();
+    if (buf == null || buf.length == 0) return null;
+    for (int i = 0; i < buf.length; i++) {
+        byte b0 = buf[i];
+        sb.append(b34enctab[(b0>>6)&0x1f]);
+        sb.append(b34enctab[(b0)&0x3]);
+        sb.append(b34enctab[(b0>>3)&0x1f]);
+
+    }
+    return sb.toString();
+}
+ 
+}
+
