58bdb35e21927756efc1b9d30cdba3d276b99ed7
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 16:17:36 -0500

    put into git at github

---
+++ pdflib/src/com/pfarrell/utils/io/ZipFileProcessor.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2011 Patrick Farrell. All Rights reserved.
+ * Will be released with a suitable open-source liscence, problably BSD-like.
+ * Contact me for a pre-release access.
+ */
+package com.pfarrell.utils.io;
+
+/**
+ * The <code>ZipFileProcessor</code> interface defines methods required to be used as a processor of Zip files
+ * in the {$link ZipWalker} class. 
+ * @author pfarrell
+ * Created on Nov 15, 2011, 6:03:51 PM
+ */
+public interface ZipFileProcessor extends ArchiveFileProcessor {
+    
+
+}
