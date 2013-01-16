32a153e7d1c35cb06782d522ce1d6e4140750952
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 16:17:36 -0500

    put into git at github

---
+++ pdflib/src/com/pfarrell/utils/io/TarFileProcessor.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2012 Patrick Farrell. All Rights reserved.
+ * Will be released with a suitable open-source liscence, problably BSD-like.
+ * Contact me for a pre-release access.
+ */
+package com.pfarrell.utils.io;
+
+/**
+ * The <code>TarFileProcessor</code> interface defines methods required to be used as a processor of Zip files
+ * in the {$link TarFileWalker} class.
+ * 
+ * @author pfarrell
+ * Created on Jun 1, 2012, 1:10:29 AM
+ */
+public interface TarFileProcessor extends ArchiveFileProcessor {
+    
+}
