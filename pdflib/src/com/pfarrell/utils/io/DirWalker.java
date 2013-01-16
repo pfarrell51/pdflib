e60a15bf8569b1166778b6e93ead636295ba7b26
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 16:17:36 -0500

    put into git at github

---
+++ pdflib/src/com/pfarrell/utils/io/DirWalker.java
@@ -0,0 +1,102 @@
+package com.pfarrell.utils.io;
+/*
+ * @(#)DirWalker.java
+ *
+ * Copyright (c) 2001, Pat Farrell.  All rights reserved.
+ * Copyright (c) 2004-2011, Pat Farrell, All rights reserved.
+ * This code will be released with a suitable Open Source
+ * license, probably BSD-like.
+ */
+
+import java.io.*;
+import org.apache.log4j.Logger;
+/**
+ * This class walks through all the files defined 
+ * by the arguments of the constructor. For all the 
+ * "files" the WalkDrivenProcessor's process() function is called.
+ * For directories, the program walk recurses.
+ */
+public class DirWalker implements Walker {
+            /** class-wide logger static for reuse   */
+protected static final Logger dwLog = Logger.getLogger(DirWalker.class);
+    
+    /** file handle to process */
+    File base;
+    /** filter to apply during processing */
+    FilenameFilter filter;
+    /** processor to apply to each directory and file */
+    WalkDrivenProcessor processor;
+    /** flag, abort/re-throw on error, or ignore exception */
+    boolean abortError = false;
+    /**
+     * construct a new DirWalker from the parameters
+     * 
+     * @param base Java file spec to process
+     * @param filter filter of files to ignore
+     * @param processor WalkDrivenProcessor to use when processing a good file
+     */
+    public DirWalker(File base, FilenameFilter filter, WalkDrivenProcessor processor) {
+        this(base, filter, processor, false);
+    }
+    /** construct a new DirWalker from the parameters
+     *
+     * @param base Java file spec to process
+     * @param filter filter of files to ignore
+     * @param processor WalkDrivenProcessor to use when processing a good file
+     * @param abortErr  boolean flag, abort/rethrow on error if true, or report error and continute if false.
+     */
+    public DirWalker(File base, FilenameFilter filter, WalkDrivenProcessor processor, boolean abortErr) {
+        this.base = base;
+        this.filter = filter;
+        this.processor = processor;
+        this.abortError = abortErr;
+    }
+    /** set abort on error flag
+     * @param arg boolean value we want to set AbortOnError control
+     */
+    @Override
+    public void setAbortOnError(boolean arg) {
+        abortError = arg;
+    }
+    /** walk thru the directory of files, processing all the good ones
+     * @throws Exception pass up any exception, usually will be runtime,
+     * such as Index out of bounds, etc.
+     */ 
+    @Override
+    public void walk() throws Exception {
+        if (!base.exists()) {
+            return;
+        }
+        if (base.isFile()) {
+            try {
+               processor.process(base);
+            } catch (Exception e) {
+                if ( abortError) {
+                    dwLog.error("Error, with abortError True", e);
+                    throw e;
+                } 
+                else
+                    dwLog.error("Error, with abortError false, skipping file", e);
+            }
+        } else {
+            if (!processor.descendDir(base)) {
+                dwLog.trace(" no descend for " + base.getAbsolutePath());
+                return;
+            }
+            File[] fList;
+            if (filter == null) {
+                fList = base.listFiles();
+            } else {
+                fList = base.listFiles(filter);
+            }
+            if (fList == null || fList.length == 0) {
+                return;
+            }
+            for (int i=0; i<fList.length; i++) {
+                DirWalker dw = new DirWalker(fList[i], filter, processor, abortError);
+                dw.walk();
+            }
+        }
+    }
+}
+
