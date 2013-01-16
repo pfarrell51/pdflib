3fc01463a9f649c5b6c837bacbf1d3b3309d2584
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 16:17:36 -0500

    put into git at github

---
+++ pdflib/src/com/pfarrell/stat/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2009 Patrick Farrell  and R. Scott Eisenberg. All Rights reserved.
+ */
+/**
+ * The Stats package contains functions to perform specialized statistical calculations such as Cluster Analysis.
+ */
+package com.pfarrell.stat;
+
