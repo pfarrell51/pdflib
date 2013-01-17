1dda54a73a03d5247e40d5d87c345448c3517b94
Pat Farrell <pfarrell@pfarrell.com>
2013-01-16 20:50:59 -0500

    add new Apache license boilerplate

--- pdflib/src/com/pfarrell/busobj/PersistentBusinessObject.java
+++ pdflib/src/com/pfarrell/busobj/PersistentBusinessObject.java
@@ -3,7 +3,18 @@
  *
  * Created on May 4, 2007, 6:14 PM
  *
- * Copyright (c) 2007, Pat Farrell All rights reserved.
+ * Copyright (c) 2007-2013, Pat Farrell All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.  
  */
 
 package com.pfarrell.busobj;
